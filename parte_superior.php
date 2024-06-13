0abf4a5be0995547bfa31a30fd102141fa68e361
Rasheed <rasheq00@gmail.com>
2024-06-12 21:44:40 -0600

    Primera clase

---
+++ parte_superior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset= "utf-8">
+    <meta name= "Proyecto tercera 1" content= "Pagina de prueba" >
+    <title>
+        Tercera 1
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+        <img src="./images/menu.png" alt="">
+</a>
+        <p class="Texto-menu-izquierda">
+            La casa de la lapa 
+
+        </p>
+
+    </div>
+</menu>
